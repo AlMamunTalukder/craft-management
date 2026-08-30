858453109e4b5452639302214b01e1dc4c834a8e
AlMamunTalukder <almamuntalukder0@gmail.com>
2026-08-30 19:28:36 +0600

    update code for studen info

--- src/app/(dashboardLayout)/dashboard/administration/page.tsx
+++ src/app/(dashboardLayout)/dashboard/administration/page.tsx
@@ -3,7 +3,9 @@ import React from 'react';
 const page = () => {
     return (
         <div>
-            <h3>administration </h3>
+
+            <div>administration</div>
+            {/* <h3>admin</h3> */}
         </div>
     );
 };
